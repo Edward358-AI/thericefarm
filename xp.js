1795b5d6453af64c4c6e29eac09b6ebba2b196eb
Edward Jiang <66029887+Edward358-AI@users.noreply.github.com>
2024-05-13 12:48:41 -0700

    Update xp.js

--- xp.js
+++ xp.js
@@ -28,9 +28,9 @@ function levelUp() {
   playerdata.xpLevel = Math.min(playerdata.xpLevel + 1);
   playerdata.xp = 0
   document.getElementById('xpLevel').innerText = `${playerdata.xpLevel}`;
-  playerdata.money += 20*parseInt(xpLevel)
-  playerdata.stats.money += 20*parseInt(xpLevel)
-  dialog("Level Up! +" + 20*parseInt(xpLevel) + "元")
+  playerdata.money += 20*playerdata.xpLevel
+  playerdata.stats.money += 20*playerdata.xpLevel
+  dialog("Level Up! +" + 20*playerdata.xpLevel + "元")
 }
 
 function updateXPBar() {
